4098226c362b850650cc37ff42e209c73ea20721
Antuan <antuanb@vt.edu>
2015-05-31 20:18:03 -0400

    html code parser and keyword permutation

--- src/j2s/ScrapeDataWithKeywords.java
+++ src/j2s/ScrapeDataWithKeywords.java
@@ -1,8 +1,11 @@
 package j2s;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import org.jsoup.nodes.Element;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -16,71 +19,41 @@ import com.googleapis.ajax.services.WebSearchQuery;
 import com.googleapis.ajax.services.enumeration.ResultSetSize;
 
 public class ScrapeDataWithKeywords {
-	
-	public static void main(String args[]) {
-		executeStackOverflowQuery(new ArrayList<String>());
-	}
 
-	//TODO: need to decide on searches per keyword or all together
-	//need to figure out how to standardize returns from searches
-	//if doing non stackoverflow (like blogs or other info) how 
-	//to strip away code and also how do we know importance?
-	public static ArrayList<String> executeStackOverflowQuery(
-			ArrayList<String> keywords) {
+	// TODO: need to decide on searches per keyword or all together
+	// need to figure out how to standardize returns from searches
+	// if doing non stackoverflow (like blogs or other info) how
+	// to strip away code and also how do we know importance?
+	public static ArrayList<String> executeStackOverflowQuery(String keywords) {
 
 		StackExchangeApiQueryFactory queryFactory = StackExchangeApiQueryFactory
 				.newInstance();
 
 		PagedList<Question> questions = queryFactory.newAdvanceSearchApiQuery()
-				.withTitle("user location")
-				.withSort(QuestionSortOrder.MOST_RELEVANT)
+				//.withUrl(url)
+				.withTitle(keywords).withSort(QuestionSortOrder.MOST_RELEVANT)
 				.withTags("ios", "swift").withFilter("WITHBODY").list();
 
 		// System.out.println(questions.size());
 		String answer = questions.get(0).getBody();
 		// System.out.println(answer);
 
-		Document doc = Jsoup.parse(answer);
-		System.out.println(doc.getElementsByTag("code").get(0).text());
-
 		ArrayList<String> searchResult = new ArrayList<String>();
 
 		return searchResult;
+		
 	}
 
-	public static HashMap<String, String> executeGoogleSearchQuery_Stack(ArrayList<String> keywords) {
+	public static HashMap<String, String> executeGoogleSearchQuery_Stack(
+			String keywords) {
 		GoogleSearchQueryFactory factory = GoogleSearchQueryFactory
 				.newInstance("applicationKey");
 		WebSearchQuery query = factory.newWebSearchQuery();
 		query.withResultSetSize(ResultSetSize.LARGE);
 		com.googleapis.ajax.common.PagedList<WebResult> response = query
-				.withQuery("location swift website:www.stackoverflow.com")
+				.withQuery(keywords + "swift website:www.stackoverflow.com")
 				.list();
-		// System.out.println(response.getCurrentPageIndex());
-		// System.out.println(response.getEstimatedResultCount());
-		// System.out.println(response.getMoreResultsUrl());
-		// System.out.println(response.getPages());
-		HashMap<String, String> googleSearchResults = new HashMap<String, String>();
-		for (WebResult result : response) {
-			String[] tokens = result.getUrl().split(
-					("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)"));
-			if (tokens[0].equals("http://stackoverflow.com/questions/")) {
-				googleSearchResults.put("title", result.getTitle());
-				googleSearchResults.put("content", result.getContent());
-				googleSearchResults.put("url", result.getUrl());
-				googleSearchResults.put("id", tokens[1]);
-			}
-		}
-		return googleSearchResults;
-	}
 
-	public static HashMap<String, String> executeGoogleSearchQuery_All(ArrayList<String> keywords) {
-		GoogleSearchQueryFactory factory = GoogleSearchQueryFactory
-				.newInstance("applicationKey");
-		WebSearchQuery query = factory.newWebSearchQuery();
-		query.withResultSetSize(ResultSetSize.LARGE);
-		com.googleapis.ajax.common.PagedList<WebResult> response = query
-				.withQuery("location swift").list();
 		HashMap<String, String> googleSearchResults = new HashMap<String, String>();
 		for (WebResult result : response) {
 			googleSearchResults.put("title", result.getTitle());
@@ -89,13 +62,63 @@ public class ScrapeDataWithKeywords {
 		}
 		return googleSearchResults;
 	}
-	
-	private String keywordCombiner(ArrayList<String> keywords) {
-		String output = "";
-		for (String s : keywords) {
-			output+=s;
-			output+=" ";
+
+	public static ArrayList<HashMap<String, String>> executeGoogleSearchQuery_All(
+			String keywords) {
+		GoogleSearchQueryFactory factory = GoogleSearchQueryFactory
+				.newInstance("applicationKey");
+		WebSearchQuery query = factory.newWebSearchQuery();
+		query.withResultSetSize(ResultSetSize.LARGE);
+		com.googleapis.ajax.common.PagedList<WebResult> response = query
+				.withQuery("location swift").list();
+		ArrayList<HashMap<String, String>> googleSearchResults = new ArrayList<HashMap<String, String>>();
+		for (WebResult result : response) {
+			String websiteURL = result.getUrl();
+			if (!websiteURL.contains("stackoverflow.com")) {
+				HashMap<String, String> googleSearchResult = new HashMap<String, String>();
+				googleSearchResult.put("title", result.getTitle());
+				googleSearchResult.put("content", result.getContent());
+				googleSearchResult.put("url", websiteURL);
+				googleSearchResults.add(googleSearchResult);
+			}
 		}
-		return output;
+		return googleSearchResults;
 	}
+
+	private static ArrayList<String> parseHTMLForCode(String input) {
+		Document doc = null;
+		if (input.startsWith("http")) {
+			try {
+				doc = Jsoup.parse(new URL(input), 0);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			doc = Jsoup.parse(input);
+		}
+		ArrayList<String> results = new ArrayList<String>();
+		for (Element result : doc.getAllElements()) {
+			if (result.tagName().equals("code")) {
+				results.add(result.text());
+			}
+			for (String className : result.classNames()) {
+				if (className.contains("code")) {
+					results.add(result.text());
+				}
+			}
+		}
+
+		return results;
+	}
+
+	public static void main(String args[]) {
+		String url = executeGoogleSearchQuery_All("").get(
+				2).get("url");
+
+		for (String code : parseHTMLForCode(url)) {
+			System.out.println(code);
+		}
+	}
+
 }
--- src/j2s/SearchAndRank.java
+++ src/j2s/SearchAndRank.java
@@ -7,20 +7,73 @@ public class SearchAndRank {
 	
 	private static ArrayList<String> queryResultStackOverflow;
 	private static HashMap<String, String> queryResultGoogleStack;
-	private static HashMap<String, String> queryResultGoogleAll;
+	private static ArrayList<HashMap<String, String>> queryResultGoogleAll;
+	private static ArrayList<String> fullKeywordSet = new ArrayList<String>();
+	private static ArrayList<String> tempKeyword = new ArrayList<String>();
 	
 	public SearchAndRank(ArrayList<String> searchKeywords) {
-		search(searchKeywords);
+		initKeywordSet(searchKeywords);
+		search();
 		rank();
 	}
 	
-	private void search(ArrayList<String> keywords) {
-		queryResultStackOverflow = ScrapeDataWithKeywords.executeStackOverflowQuery(keywords);
-		queryResultGoogleStack = ScrapeDataWithKeywords.executeGoogleSearchQuery_Stack(keywords);
-		queryResultGoogleAll = ScrapeDataWithKeywords.executeGoogleSearchQuery_All(keywords);
+	private static void initKeywordSet(ArrayList<String> searchKeywords) {
+		for (int i = 0; i < searchKeywords.size(); i++) {
+			fullKeywordSet.add(searchKeywords.get(i));
+		}
+		permutation(searchKeywords);
+		for (int i = 0; i < tempKeyword.size(); i++) {
+			fullKeywordSet.add(tempKeyword.get(i).trim());
+		}
+		if (searchKeywords.size() > 1) {
+			fullKeywordSet.add(searchKeywords.get(0) + " " + searchKeywords.get(1));
+			fullKeywordSet.add(searchKeywords.get(1) + " " + searchKeywords.get(0));
+		}
+	}
+	
+	private void search() {
+		for (String keyword : fullKeywordSet) {
+			queryResultStackOverflow = ScrapeDataWithKeywords.executeStackOverflowQuery(keyword);
+			queryResultGoogleStack = ScrapeDataWithKeywords.executeGoogleSearchQuery_Stack(keyword);
+			queryResultGoogleAll = ScrapeDataWithKeywords.executeGoogleSearchQuery_All(keyword);
+		}
+		
 	}
 	
 	private void rank() {
 		
+	}
+	
+	//testing purposes
+	public static void main(String args[]) {
+		ArrayList<String> test = new ArrayList<String>();
+		test.add("location");
+		test.add("user");
+		test.add("get");
+		initKeywordSet(test);
+		System.out.println(fullKeywordSet.toString());
+	}
+	
+	public static void permutation(ArrayList<String> str) { 
+	    permutation("", str); 
+	}
+
+	private static void permutation(String prefix, ArrayList<String> str) {
+	    int n = str.size();
+	    if (n == 0) {
+	    	tempKeyword.add(prefix);
+	    }
+	    else {
+	        for (int i = 0; i < n; i++) {
+	        	ArrayList<String> temp = new ArrayList<String>();
+	        	for (int j = 0; j < i; j++) {
+	        		temp.add(str.get(j));
+	        	}
+	        	for (int j = i+1; j < n; j++) {
+	        		temp.add(str.get(j));
+	        	}	        	
+	            permutation(prefix + str.get(i) + " ", temp);
+	        }
+	    }
 	}
 }
