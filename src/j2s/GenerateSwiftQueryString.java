b2c8965a02764c226907ba97ab0bff8801fb50bb
Antuan <antuanb@vt.edu>
2015-06-06 20:24:52 -0400

    % change on both sides

--- src/j2s/GenerateSwiftQueryString.java
+++ src/j2s/GenerateSwiftQueryString.java
@@ -43,6 +43,7 @@ public class GenerateSwiftQueryString {
 		filterKeys.add("!");
 		filterKeys.add("\\");
 		filterKeys.add("\"");
+		filterKeys.add("%");
 	}
 
 	public ArrayList<String> executeFrequencyAnalysis(String filepath) {
